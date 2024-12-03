4f87171e1462f71481e414524c491723c321c58d
Asad9340 <asadulimran1999@gmail.com>
2024-12-03 12:59:38 +0600

    update project display

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import './Projects.css';
 const Projects = () => {
   return (
-    <div className="bg-primary py-8 md:py-12 mb-6">
+    <div className="bg-primary py-8 md:py-12 mb-6 font-lexend">
       <div className="max-w-6xl mx-auto">
         <div className="text-center text-white">
           <span>- Projects</span>
@@ -17,25 +17,28 @@ const Projects = () => {
               alt="Projects Image"
             />
           </div>
-          <div className="flex flex-col justify-between">
-            <div className="flex gap-3 items-center ">
-              <button className="keywordBtn"> Web Development</button>
+          <div>
+            <div className="flex gap-4 md:gap-5 items-center mb-4 md:mb-6">
+              <button className="keywordBtn"> Web Dev</button>
               <button className="keywordBtn">Responsive Design</button>
               <button className="keywordBtn">API Integration</button>
             </div>
-            <div>
-              <h2>
-                Project Title: <span>Stay Vista</span>
+            <div className="my-4 md:my-6">
+              <h2 className="text-3xl md:text-4xl ">
+                Project:{' '}
+                <span className="font-semibold md:font-bold">
+                  <span className="text-[#DC143C]">S</span>tay Vista
+                </span>
               </h2>
             </div>
-            <div>
+            <div className="my-4 md:my-6">
               <p>
                 Stay Vista is a web application that helps users find the best
                 accommodations in various locations. It features a search
                 engine, booking system, and a user-friendly interface.
               </p>
             </div>
-            <div>
+            <div className="my-4 md:my-6">
               <button className="viewMore learn-more border">
                 <span className="circle" aria-hidden="true">
                   <span className="icon arrow"></span>
